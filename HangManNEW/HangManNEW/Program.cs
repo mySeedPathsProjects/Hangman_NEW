e9a3d4e299c843262a989f3776932aee8947fa05
nate stephens <ndstephens@gmail.com>
2015-02-06 01:02:02 -0700

    almost done
    animation added.  thought ending game bugs were fixed, but now back.

--- HangManNEW/HangManNEW/Program.cs
+++ HangManNEW/HangManNEW/Program.cs
@@ -10,65 +10,29 @@ namespace HangManNEW
     class Program
     {
         //GLOBAL VARIABLES
-        static int GuessRemaining = 8;
-        static int pauseDuration = 1000;
-        static int cursorX = Console.WindowWidth;
-        static int cursorY = Console.WindowHeight;
+        static int GuessesGiven = 7;
+        static int GuessRemaining = 0;
+        static int PauseDuration = 1000;
+        static int CursorX = Console.WindowWidth;
+        static int CursorY = Console.WindowHeight;
 
+        static bool IsPlaying = true;
         static Random rng = new Random();
         static List<string> wordBank = new List<string>() { "puzzling", "trucking", "haphazard", "jawbreaker", "kazoo", "nowadays", "numbskull", "rhubarb", "jigsaw" };
         static string WordToGuess = string.Empty;
 
         static string LettersGuessed = string.Empty;
-        static string WordFromLetters = string.Empty;
+        static string MaskedWord = string.Empty;
 
-        static bool IsPlaying = true;
 
 
 
         static void Main(string[] args)
         {
-
-            //Greeting();
-
-        }
-
-        static void Greeting()
-        {
-            Console.CursorVisible = false;
-            string name = string.Empty;
-
-            Thread.Sleep(pauseDuration * 2);
-
-            Console.SetCursorPosition(cursorX / 4, cursorY / 2);
-            OldTimeyTextPrinter("Hey man.  ", 50);
-            Thread.Sleep(pauseDuration * 2);
-            OldTimeyTextPrinter("So what's your name? ", 30);
-            name = Console.ReadLine();
-            Thread.Sleep(pauseDuration);
-
-            Console.SetCursorPosition(cursorX / 4, cursorY / 2);
-            OldTimeyTextPrinter("        Cool                  ", 50);
-            Thread.Sleep(pauseDuration / 2);
-            Console.SetCursorPosition((cursorX / 4) + 12, cursorY / 2);
-            OldTimeyTextPrinter("  nice to meet you", 50);
-            Thread.Sleep(pauseDuration * 2);
-
-            Console.SetCursorPosition(cursorX / 4, cursorY / 2);
-            OldTimeyTextPrinter("                                    ", 10);
-            Console.SetCursorPosition(cursorX / 4, cursorY / 2);
-            OldTimeyTextPrinter("   Have you ever heard of the game  ", 60);
-            Thread.Sleep(pauseDuration / 2);
-            OldTimeyTextPrinter(".  .  .  ", 200);
-
-    //ANIMATION FUNCTION (keep it simple)
-            //IntroAnimation();
-
-            Thread.Sleep(pauseDuration * 3);
-
-    //****ADD MORE EXPLANATION***
-
-
+            Console.SetWindowSize(70, 35);
+            Greeting();
+            IntroAnimation();
+            RunGame();
         }
 
         static void OldTimeyTextPrinter(string inputText, int pauseDuration)
@@ -83,85 +47,466 @@ namespace HangManNEW
             }
         }
 
+        static void Greeting()
+        {
+            Console.CursorVisible = false;
+            string name = string.Empty;
+
+            Thread.Sleep(PauseDuration);
+            Console.SetCursorPosition(CursorX / 5, CursorY / 2);
+            OldTimeyTextPrinter("Hey kid.  ", 50);
+            Thread.Sleep(PauseDuration * 2);
+            OldTimeyTextPrinter("So what's your name? ", 30);
+            name = Console.ReadLine();
+            Thread.Sleep(PauseDuration);
+
+            Console.SetCursorPosition(CursorX / 5, CursorY / 2);
+            OldTimeyTextPrinter("        Cool                  ", 50);
+            Thread.Sleep(PauseDuration / 4);
+            Console.SetCursorPosition((CursorX / 5) + 12, CursorY / 2);
+            OldTimeyTextPrinter("  nice to meet you", 50);
+            Thread.Sleep(PauseDuration * 2);
+
+            Console.SetCursorPosition(CursorX / 5, CursorY / 2);
+            OldTimeyTextPrinter("                                    ", 10);
+            Console.SetCursorPosition(CursorX / 5, CursorY / 2);
+            OldTimeyTextPrinter("   Have you ever heard of the game  ", 60);
+            Thread.Sleep(PauseDuration / 2);
+            OldTimeyTextPrinter(".  .  .  ", 200);
+        }
+       
         static void IntroAnimation()
         {
-    //****NEED TO MAKE***
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.SetCursorPosition(0, 8);
+            string cat = @"
+ ██░ ██  ▄▄▄       ███▄    █   ▄████     ▄████▄   ▄▄▄     ▄▄▄█████▓
+▓██░ ██▒▒████▄     ██ ▀█   █  ██▒ ▀█▒   ▒██▀ ▀█  ▒████▄   ▓  ██▒ ▓▒
+▒██▀▀██░▒██  ▀█▄  ▓██  ▀█ ██▒▒██░▄▄▄░   ▒▓█    ▄ ▒██  ▀█▄ ▒ ▓██░ ▒░
+░▓█ ░██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█  ██▓   ▒▓▓▄ ▄██▒░██▄▄▄▄██░ ▓██▓ ░ 
+░▓█▒░██▓ ▓█   ▓██▒▒██░   ▓██░░▒▓███▀▒   ▒ ▓███▀ ░ ▓█   ▓██▒ ▒██▒ ░ 
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ░▒   ▒    ░ ░▒ ▒  ░ ▒▒   ▓▒█░ ▒ ░░   
+ ▒ ░▒░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░  ░   ░      ░  ▒     ▒   ▒▒ ░   ░    
+ ░  ░░ ░  ░   ▒      ░   ░ ░ ░ ░   ░    ░          ░   ▒    ░      
+ ░  ░  ░      ░  ░         ░       ░    ░ ░            ░  ░        
+                                        ░                          
+            ";
+            OldTimeyTextPrinter(cat, 3);
+            Thread.Sleep(PauseDuration);
+            Console.WriteLine();
+            Console.WriteLine();
+            OldTimeyTextPrinter("     I'm going to pick a random word...", 40);
+            Thread.Sleep(PauseDuration / 2);
+            Console.WriteLine();
+            OldTimeyTextPrinter("          Win the game by guessing correct letters", 40);
+            Thread.Sleep(PauseDuration * 2);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("           OR THE CAT GETS IT!!!");
+            Console.WriteLine();
+            Thread.Sleep(PauseDuration * 3);
         }
+
 
         static void RunGame()
         {
+            WordToGuess = wordBank[rng.Next(wordBank.Count)].ToUpper();
+            GuessRemaining = GuessesGiven;
+            LettersGuessed = string.Empty;
             IsPlaying = true;
-            WordToGuess = wordBank[rng.Next(wordBank.Count)];
 
             while (IsPlaying)
             {
-                if (GuessRemaining > 0)
+                Console.Clear();
+
+         //**** FOR TESTING ****
+                Console.WriteLine(WordToGuess);
+                //Console.WriteLine();
+         //**** FOR TESTING ****
+
+                BuildMaskedWord();
+                PrintRoundInfo();
+                Console.Write("Please enter a letter:");
+                string letterGuess = Console.ReadLine();
+                if (ValidateLetter(letterGuess))
                 {
-                    if (WordGuessed(WordToGuess, WordFromLetters))
-                    {
-                        YouWon();
-                    }
-                    else
-                    {
-                        //RoundInfo(GuessRemaining, 
-                        //get info
-                    }
+                    IsLetterInWord(letterGuess);
                 }
-                //run out of guesses
-                else
+                //BuildMaskedWord() here a second time b/c of bug i couldn't figure out in time...
+                BuildMaskedWord();
+                if (GuessRemaining == 0 || WordGuessed(WordToGuess, MaskedWord))
                 {
-                    YouLose();
+                    IsPlaying = false;
                 }
             }
-
+            DidYouWin();
         }
 
-
-        static bool WordGuessed(string wordToGuess_, string wordFromLetters_)
+        static void DidYouWin()
         {
-            if (wordToGuess_.Replace(" ", string.Empty) == wordFromLetters_.Replace(" ", string.Empty))
+            if (WordGuessed(WordToGuess, MaskedWord))
+            {
+                YouWon();
+            }
+            YouLose();
+        }
+
+        static bool WordGuessed(string wordToGuess_, string maskedWord_)
+        {
+            if (wordToGuess_.ToUpper() == maskedWord_.ToUpper().Replace(" ", string.Empty))
             {
                 return true;
             }
             return false;
         }
 
-        private static void YouWon()
+        static bool ValidateLetter(string letter)
         {
-            //Graphic(number);
-            Console.WriteLine("Congrats");
-            Console.WriteLine("do you want to play again?  y or n");
-            string answer = Console.ReadLine();
-            if (answer.ToLower() == "y")
+            if (letter.ToUpper() == WordToGuess.ToUpper())
             {
-                LettersGuessed = string.Empty;
-                RunGame();
+                YouWon();
             }
-            IsPlaying = false;
+            else if (letter.Length != 1)
+            {
+                Console.WriteLine();
+                OldTimeyTextPrinter("INVALID INPUT...ENTER A SINGLE LETTER", 20);
+                Thread.Sleep(PauseDuration*2);
+                return false;
+            }
+            else if (LettersGuessed.ToUpper().Contains(letter.ToUpper()))
+            {
+                Console.WriteLine();
+                OldTimeyTextPrinter("YOU ALREADY TRIED THIS LETTER", 20);
+                Thread.Sleep(PauseDuration*2);
+                return false;
+            }
+            else if (char.IsLetter(letter[0]))
+            {
+                return true;
+            }
+            return false;
         }
 
-        private static void YouLose()
+        static void IsLetterInWord(string letter)
         {
-            //Graphic(number);
-            Console.WriteLine("better luck next time");
-            Console.Write("do you want to play again?  y or n");
-            string answer = Console.ReadLine();
-            if (answer.ToLower() == "y")
+            if (WordToGuess.Contains(letter.ToUpper()))
             {
-                LettersGuessed = string.Empty;
-                RunGame();
+                LettersGuessed += letter.ToUpper();
+                Console.WriteLine();
+                OldTimeyTextPrinter("CORRECT!!  GOOD CHOICE!", 20);
+                Thread.Sleep(PauseDuration);
             }
-            IsPlaying = false;
+            else
+            {
+                LettersGuessed += letter.ToUpper();
+                Console.WriteLine();
+                OldTimeyTextPrinter("NOPE!!  NOT THIS LETTER!", 20);
+                Thread.Sleep(PauseDuration);
+                GuessRemaining--;
+            }
+
         }
 
-
-
-
-    //****HAVE TO MAKE GRAPHICS OF MAN HANGING****
-        private static void Graphic(int switchNumber)
+        static void BuildMaskedWord()
         {
-    //****MAKE A SWITCH STATEMENT*****
-            throw new NotImplementedException();
+            MaskedWord = string.Empty;
+            for (int i = 0; i < WordToGuess.Length; i++)
+            {
+                if (LettersGuessed.Contains(WordToGuess[i].ToString()))
+                {
+                    MaskedWord += WordToGuess[i] + " ";
+                }
+                else
+                {
+                    MaskedWord += "_ ";
+                }
+            }
+        }
+
+        static void PrintRoundInfo()
+        {
+            Graphic(GuessesGiven - GuessRemaining);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(MaskedWord);
+            Console.WriteLine();
+            Console.Write("GUESSES REMAINING: " + GuessRemaining);
+            Console.WriteLine("     LETTERS GUESSED: " + LettersGuessed);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void YouWon()
+        {
+            Console.Clear();
+            Graphic(8);
+            Console.WriteLine();
+            Console.WriteLine();
+            OldTimeyTextPrinter("THANKS BUD!", 60);
+            Console.WriteLine();
+            Thread.Sleep(PauseDuration);
+            OldTimeyTextPrinter("I'LL SEE YA AROUND", 60);
+            Console.WriteLine();
+            Console.WriteLine();
+            Thread.Sleep(PauseDuration * 2);
+            OldTimeyTextPrinter("PLAY AGAIN?   Y or N", 60);
+            Console.WriteLine();
+            if (Console.ReadLine().ToUpper() == "Y")
+            {
+                RunGame();
+            }
+        }
+
+        static void YouLose()
+        {
+            Console.Clear();
+            Graphic(7);
+            Console.WriteLine();
+            Console.WriteLine();
+            OldTimeyTextPrinter("THAT'S PRETTY MESSED UP", 60);
+            Console.WriteLine();
+            Thread.Sleep(PauseDuration);
+            OldTimeyTextPrinter("BETTER LUCK NEXT TIME", 60);
+            Console.WriteLine();
+            Console.WriteLine();
+            Thread.Sleep(PauseDuration * 2);
+            OldTimeyTextPrinter("PLAY AGAIN?   Y or N", 60);
+            Console.WriteLine();
+            if (Console.ReadLine().ToUpper() == "Y")
+            {
+                RunGame();
+            }
+        }
+
+        static void Graphic(int switchNumber)
+        {
+            switch (switchNumber)
+            {
+                case 0:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||           //\\
+||          ||  ||
+||          \\  // 
+||           ^^^^
+||           
+||          
+||          
+||         
+||               
+||             
+||              
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 1:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||          |\_/|
+||         =)'T'(= 
+||           `'` 
+||          
+||           
+||          
+||         
+||          
+||             
+||              
+||              
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 2:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||          |\_/|
+||         =)'T'(= 
+||          /`'` 
+||          \) 
+||           
+||         
+||         
+||         
+||              
+||              
+||              
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 3:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||          |\_/|
+||         =)'T'(= 
+||          /`'`\  
+||          \) (/
+||           
+||         
+||         
+||         
+||             
+||             
+||              
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 4:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||          |\_/|
+||         =)'T'(= 
+||          /`'`\  
+||          \) (/
+||           | |
+||         
+||         
+||          
+||              
+||             
+||               
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 5:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||          |\_/|
+||         =)'T'(= 
+||          /`'`\  
+||          \) (/
+||           | |
+||          /\ 
+||          \ T 
+||          (/ 
+||               
+||              
+||              
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 6:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||            \\
+||            //
+||          |\_/|
+||         =)'T'(= 
+||          /`'`\  
+||          \) (/
+||           | |
+||          /\ /\
+||          \ T /
+||          (/ \)
+||               
+||             
+||              
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 7:
+                    Console.WriteLine(@"
+ _______________________
+||            \\
+||            //
+||           ___
+||    ,_    '---'    _,
+||    \ `-._|\_/|_.-' /
+||     |   =)'T'(=   |
+||      \   /`'`\   /
+||       '._\) (/_.'
+||           | |
+||          /\ /\
+||          \ T /
+||          (/ \)\
+||               ))
+||              ((
+||               \)
+||___________________________
+-----------------------------
+[][]                     [][]
+");
+                    break;
+                case 8:
+                    Console.WriteLine(@"
+
+
+           /\
+            \ \
+             \ \
+             / /
+            / /
+           _\ \_/\/\
+          /  *  \@@ =
+         |       |Y/
+         |       |~
+          \ /_\ /
+           \\ //
+            |||
+           _|||_
+          ( / \ )
+");
+                    break;
+                case 9:
+                    Console.WriteLine(@"
+ ██░ ██  ▄▄▄       ███▄    █   ▄████     ▄████▄   ▄▄▄     ▄▄▄█████▓
+▓██░ ██▒▒████▄     ██ ▀█   █  ██▒ ▀█▒   ▒██▀ ▀█  ▒████▄   ▓  ██▒ ▓▒
+▒██▀▀██░▒██  ▀█▄  ▓██  ▀█ ██▒▒██░▄▄▄░   ▒▓█    ▄ ▒██  ▀█▄ ▒ ▓██░ ▒░
+░▓█ ░██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█  ██▓   ▒▓▓▄ ▄██▒░██▄▄▄▄██░ ▓██▓ ░ 
+░▓█▒░██▓ ▓█   ▓██▒▒██░   ▓██░░▒▓███▀▒   ▒ ▓███▀ ░ ▓█   ▓██▒ ▒██▒ ░ 
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ░▒   ▒    ░ ░▒ ▒  ░ ▒▒   ▓▒█░ ▒ ░░   
+ ▒ ░▒░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░  ░   ░      ░  ▒     ▒   ▒▒ ░   ░    
+ ░  ░░ ░  ░   ▒      ░   ░ ░ ░ ░   ░    ░          ░   ▒    ░      
+ ░  ░  ░      ░  ░         ░       ░    ░ ░            ░  ░        
+                                        ░                          
+");
+                    break;
+
+                default:
+                    break;
+            }
+            
         }
     }
 }
